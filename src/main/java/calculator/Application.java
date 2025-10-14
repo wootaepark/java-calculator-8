f5e3c114ad1b1cd5c60d6260c10a8d3b7a0b7c4d
wootaepark <pakdal99@gmail.com>
2025-10-14 17:52:35 +0900

    feat(calculator): split numbers using ',' or ':'

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 문자열 입력
+        System.out.print("input String : ");
+        String input_string = Console.readLine(); // ex : "1,2:3"
+
+        String[] numbers = input_string.split("[,:]"); // , 또는 : 문자로 문자열 분해
+        System.out.println(Arrays.toString(numbers));
+
+
     }
+
+
 }
