5bbe130a037a093b3e67ead848f8b4d00b5fbc88
wootaepark <pakdal99@gmail.com>
2025-10-14 17:55:44 +0900

    feat(calculator): handle empty input and sum numbers by ',' or ':'

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,17 +2,25 @@ package calculator;
 
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
+
+        long sum = 0;
 
         // 문자열 입력
         System.out.print("input String : ");
         String input_string = Console.readLine(); // ex : "1,2:3"
 
-        String[] numbers = input_string.split("[,:]"); // , 또는 : 문자로 문자열 분해
-        System.out.println(Arrays.toString(numbers));
+        if (!input_string.isEmpty()) {
+            String[] numbers = input_string.split("[,:]"); // , 또는 : 문자로 문자열 분해
+            // System.out.println(Arrays.toString(numbers));
+            for (String number : numbers) {
+                sum += Integer.parseInt(number);
+            }
+        }
+
+        System.out.println("result = " + sum);
 
 
     }
