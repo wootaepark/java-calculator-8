b46df39d73e49130af1deeaf73a378ff4fdfcb74
wootaepark <pakdal99@gmail.com>
2025-10-15 13:44:09 +0900

    feat(calculator): add custom delimiter parsing using '//' and '\n'

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,12 +12,33 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요");
         String input_string = Console.readLine(); // ex : "1,2:3"
 
+        // 비어 있지 않은 경우 계산식 수행
         if (!input_string.isEmpty()) {
-            String[] numbers = input_string.split("[,:]"); // , 또는 : 문자로 문자열 분해
-            // System.out.println(Arrays.toString(numbers));
-            for (String number : numbers) {
-                sum += Integer.parseInt(number);
+
+            // 정규 표현식 : // 뒤 에서 \\n 앞까지의 문자열을 찾는다.
+            if (input_string.contains("//") && input_string.contains("\\n")) {
+                
+                char separator = input_string.charAt(input_string.indexOf("//") + 2);
+
+                String sub = input_string.substring(input_string.indexOf("\\n") + 2);
+
+                String[] numbers = sub.split(String.valueOf(separator));
+
+                for (String number : numbers) {
+                    sum += Long.parseLong(number);
+                }
+
+
+            } else {
+                // case 1 : , : 구분자
+                String[] numbers = input_string.split("[,:]"); // , 또는 : 문자로 문자열 분해
+
+                for (String number : numbers) {
+                    sum += Long.parseLong(number);
+                }
             }
+
+
         }
 
         System.out.println("결과 : " + sum);
