d63e75209728b923fbc6074f5646ac6f1c5988f3
wootaepark <pakdal99@gmail.com>
2025-10-15 16:34:28 +0900

    refactor(calculator): move calculation logic to calculator() for clarity

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,58 +6,40 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
 
-        long sum = 0;
-
         // 문자열 입력
         System.out.println("덧셈할 문자열을 입력해 주세요");
         String input_string = Console.readLine(); // ex : "1,2:3"
 
-        // 비어 있지 않은 경우 계산식 수행
-
-        if (!input_string.isEmpty()) {
-
-            // 정규 표현식 : // 뒤 에서 \\n 앞까지의 문자열을 찾는다.
-            if (input_string.contains("//") && input_string.contains("\\n")) {
-
-                char separator = input_string.charAt(input_string.indexOf("//") + 2);
-
-                String sub = input_string.substring(input_string.indexOf("\\n") + 2);
-
-                // 비슷함 a
-                String[] numbers = sub.split(String.valueOf(separator));
-
-                // 중복 a
-                for (String number : numbers) {
-                    long op = Long.parseLong(number);
-                    if (op <= 0) {
-                        throw new IllegalArgumentException("양의 정수만 입력이 가능합니다.");
-                    }
-                    sum += op;
-                }
-
-
-            } else {
-                // case 1 : , : 구분자
-                // 비슷함 a
-                String[] numbers = input_string.split("[,:]"); // , 또는 : 문자로 문자열 분해
-
-                // 중복 a
-                for (String number : numbers) {
-                    long op = Long.parseLong(number);
-                    if (op <= 0) {
-                        throw new IllegalArgumentException("양의 정수만 입력이 가능합니다.");
-                    }
-                    sum += op;
-                }
-            }
-
-
-        }
-
-        System.out.println("결과 : " + sum);
+        System.out.println("결과 : " + calculator(input_string));
 
 
     }
 
+    private static long calculator(String input_string) {
+        if (input_string.isEmpty()) {
+            return 0;
+        }
+
+        String[] numbers;
+        if (input_string.contains("//") && input_string.contains("\\n")) {
+            String[] parts = input_string.split("\\\\n");
+            char separator = parts[0].charAt(2); // //;\n 에서 ; 추출
+            numbers = parts[1].split(String.valueOf(separator));
+        } else {
+            numbers = input_string.split("[,:]");
+
+        }
+        long sum = 0;
+        for (String number : numbers) {
+            long op = Long.parseLong(number);
+            if (op <= 0) {
+                throw new IllegalArgumentException("양의 정수만 입력이 가능합니다.");
+            }
+            sum += op;
+
+        }
+        return sum;
+    }
+    
 
 }
