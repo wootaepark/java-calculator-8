d0f37f006658ec4fce24a42cf54046b9b73a2da2
wootaepark <pakdal99@gmail.com>
2025-10-15 14:04:08 +0900

    feat(calculator): throw IllegalArgumentException on invalid input and exit program

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -13,28 +13,41 @@ public class Application {
         String input_string = Console.readLine(); // ex : "1,2:3"
 
         // 비어 있지 않은 경우 계산식 수행
+
         if (!input_string.isEmpty()) {
 
             // 정규 표현식 : // 뒤 에서 \\n 앞까지의 문자열을 찾는다.
             if (input_string.contains("//") && input_string.contains("\\n")) {
-                
+
                 char separator = input_string.charAt(input_string.indexOf("//") + 2);
 
                 String sub = input_string.substring(input_string.indexOf("\\n") + 2);
 
+                // 비슷함 a
                 String[] numbers = sub.split(String.valueOf(separator));
 
+                // 중복 a
                 for (String number : numbers) {
-                    sum += Long.parseLong(number);
+                    long op = Long.parseLong(number);
+                    if (op <= 0) {
+                        throw new IllegalArgumentException("양의 정수만 입력이 가능합니다.");
+                    }
+                    sum += op;
                 }
 
 
             } else {
                 // case 1 : , : 구분자
+                // 비슷함 a
                 String[] numbers = input_string.split("[,:]"); // , 또는 : 문자로 문자열 분해
 
+                // 중복 a
                 for (String number : numbers) {
-                    sum += Long.parseLong(number);
+                    long op = Long.parseLong(number);
+                    if (op <= 0) {
+                        throw new IllegalArgumentException("양의 정수만 입력이 가능합니다.");
+                    }
+                    sum += op;
                 }
             }
 
